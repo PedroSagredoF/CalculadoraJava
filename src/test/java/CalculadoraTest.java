228cfecf12f5a783483446ca91dfa5f1b8ceefae
Pedro Sagredo <pedro.sagredof@gmail.com>
2021-06-10 01:44:13 -0400

    Se agrega funcion assertAll

--- src/test/java/CalculadoraTest.java
+++ src/test/java/CalculadoraTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,21 +9,29 @@ class CalculadoraTest {
 
     @Test
     void testAdd() {
-        assertEquals(6,calc.add(3,3));
+        Assertions.assertAll(()-> assertEquals(6,calc.add(3,3)),
+                             ()-> assertEquals(18,calc.add(10,8))
+        );
     }
 
     @Test
     void testRes() {
-        assertEquals(0,calc.res(3,3));
+        Assertions.assertAll(() -> assertEquals(0,calc.res(3,3)),
+                             () -> assertEquals(5,calc.res(8,3))
+        );
     }
 
     @Test
     void testMul() {
-        assertEquals(6,calc.mul(2,3));
+        Assertions.assertAll(() -> assertEquals(6,calc.mul(2,3)),
+                             () -> assertEquals(9,calc.mul(3,3))
+        );
     }
 
     @Test
     void testDiv() {
-        assertEquals(6,calc.div(18,3));
+        Assertions.assertAll(() -> assertEquals(6,calc.div(18,3)),
+                             () -> assertEquals(7,calc.div(21,3))
+        );
     }
 }
