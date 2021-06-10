d8ac4b5c85090896e1180ac86f531bb844c90588
Pedro Sagredo <pedro.sagredof@gmail.com>
2021-06-10 00:44:48 -0400

    Se completa metodo main, con menu de opciones y ejecucion de los metodos

--- src/main/java/Calculadora.java
+++ src/main/java/Calculadora.java
@@ -1,25 +1,62 @@
+import java.util.Scanner;
+
 public class Calculadora {
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Elige la Operación");
+        System.out.println("1 Para Suma");
+        System.out.println("2 Para Resta");
+        System.out.println("3 Para Multiplicación");
+        System.out.println("4 Para División");
+
+        int choose = scan.nextInt();
+        System.out.println("Ingresa 2 Números");
+        int x = scan.nextInt();
+        int y = scan.nextInt();
+
+        if (choose >4){
+            System.out.println("El rango a elegir debe ser entre 1 y 4");
+        }else{
+            switch (choose)
+            {
+                case 1: System.out.println("La respuesta es: "+ add(x,y));
+                    break;
+
+                case 2: System.out.println("La respuesta es: "+ res(x,y));
+                    break;
+
+                case 3: System.out.println("La respuesta es: "+ mul(x,y));
+                    break;
+
+                case 4:
+                    System.out.println("La respuesta es: "+ div(x,y));
+                    break;
+            }
+
+
+        }
+
 
     }
 
-    public static int add (x,y){
+    public static int add (int x, int y){
         int res=x+y;
         return res;
     }
 
-    public static int res (x,y){
+    public static int res (int x, int y){
         int res=x-y;
         return res;
     }
 
-    public static int mul (x,y){
+    public static int mul (int x, int y){
         int res=x*y;
         return res;
     }
 
-    public static int div (x,y){
+    public static int div (int x, int y){
         int res=x/y;
         return res;
     }
