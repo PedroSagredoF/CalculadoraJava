9a07102277886a91182b114a2d00653e2f180686
Pedro Sagredo <pedro.sagredof@gmail.com>
2021-06-10 00:25:03 -0400

    Primer Commit: Se crea Clase Calculadora, y los metodos para cada operacion

---
+++ src/main/java/Calculadora.java
@@ -0,0 +1,27 @@
+public class Calculadora {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int add (x,y){
+        int res=x+y;
+        return res;
+    }
+
+    public static int res (x,y){
+        int res=x-y;
+        return res;
+    }
+
+    public static int mul (x,y){
+        int res=x*y;
+        return res;
+    }
+
+    public static int div (x,y){
+        int res=x/y;
+        return res;
+    }
+
+}
